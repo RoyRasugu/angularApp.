d3ee2dbc58bce03033f5334844746d5bdc1dd099
Roy Rasugu <roy.rasugu@student.moringaschool.com>
2021-08-23 11:24:40 +0300

    make dem changes

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -8,7 +8,7 @@ import { Quote } from '../quote';
 })
 export class QuoteComponent implements OnInit {
   quotes:Quote[] = [
-    new Quote(1, 'Brian', 'Living Life', 'Live as if you were to die tomorrow. Learn as if you were to live forever','Milton Berle',new Date(2021,7,23),0,0),
+    new Quote(1, 'Klein', 'Living Life', 'Live as if you were to die tomorrow. Learn as if you were to live forever','Milton Berle',new Date(2021,7,23),0,0),
     new Quote(2, 'Katie', 'Personal Feelings', 'Be who you are and say what you feel, because those who mind don’t matter and those who matter don’t mind','Napoleon Hill',new Date(2021,6,17),0,0),
     new Quote(3, 'Catherine', 'Human Perceptions', 'We must not allow other people’s limited perceptions to define us.','Oscar Wilde',new Date(2021,1,12),0,0),
     new Quote(4, 'Wendell', 'Greatness', 'If you cannot do great things, do small things in a great way','Virginia Satir',new Date(2021,2,14),0,0),
